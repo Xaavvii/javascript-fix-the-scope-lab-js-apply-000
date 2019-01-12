97c098c2fe60e0335b87973362306f2923e5fd98
authentic-actor-8801 <xaviernational@aol.com>
2019-01-12 18:43:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,11 +12,12 @@ function yourAnimal() {
   return animal
 }
 
-function add2(n) {
-  return n + two
-
+function add2(n) 
+{
   // Feel free to move things around!
   const two = 2
+  
+  return n + two
 }
 
 var funkyFunction = function() {
